7c029a697e09f7c07231947e4ff8f9bb583f0d63
Salman-Khan-py <imsalmanay2004@gmail.com>
2025-06-01 15:27:04 +0500

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,203 @@
+print("Hello, World!")
+print("Name: Salman Khan, Age: 21, City: Nowshera")
+print("welcome to pytho!\nLet's learn coding")
+print('He said, "Python is Amazing!"')
+
+print("*\n* *\n* * *\n* * * *\n* * * * *")
+
+Name="Salman Khan"
+Age="21"
+print("Hello, my name is",Name,"and I am ",Age,"years Old")
+
+for i in range(1,6,1):
+    print("5 x ",i,"=",5*i)
+
+print("Apple\tBanana\tCherry")
+
+num_1=5
+num_2=7
+print(num_1,"+",num_2,"=",num_1+num_2)
+
+print("Item Names:\nQuantity:\nPrice:\nTotal:\n####################")
+
+print("The 10 keywords:\tfor\tdo\twhile\tif\tasync\tawait\telse\treturn\tbreak\tnot\n#################")
+
+print("Conditional statement Keywords: if\telse\telif\tswitch\n####################")
+
+print("Keyword used in loops: for\twhile\tdo\tbreak\tcontinue\n##################")
+
+print("keywords in loops: for\twhile\tdo\tbreak\tcontinue\n##################")
+
+print("Boolean Keywords: true\tfalse\tand\tor\tnot\tis\n##################")
+
+print("Constant values keywords: true\tfalse\tnone\n##################")
+
+print("Flow control keywords: break\tcontinue\tpass\n##################")
+
+print("The keyword 'if' is used to check a conditions.\n##################")
+
+print("keyword |\tUse\nif\t\tConditional check\nwhile\tLoop\nbreak\tExit loop\n##################")
+
+capital_keywords="if,else,while"
+print(capital_keywords.upper(),"\n##################")
+
+keywords_list=['if','else','for','while','do','break','continue','break','exit','false']
+print(keywords_list,"\n##################")
+
+print("The keyword cannot be used as variable name: if\twhile\tfor\tclass\tdef\n##################")
+
+print("python uses if, else, and for to control the flow of code\n##################")
+
+print("Conditional\tLooping\tBoolean\nif\t\t\tfor\t\ttrue\nelse\t\twhile\tfalse\nelif\t\tdo\n##################")
+
+print("we can't use the keywords as variable names because keywords are the reserved words,\n means using keyword as a variable name would create a syntax conflict,\n##################")
+
+number_1="10"
+output_1=int(number_1)
+print(output_1+5,"\n##################")
+
+number_2="7.2"
+output_2=float(number_2)
+print(output_2,"\n##################")
+
+number_3=5
+cnvrt_str=str(number_3)
+print("the number  is "+cnvrt_str,"\n##################")
+
+number_3=3.14
+cnvrt_flt=int(number_3)
+print(cnvrt_flt,"\n##################")
+
+number_4=1
+cnvrt_bol=bool(number_4)
+print(cnvrt_bol,"\n##################")
+
+word_1="False"
+cnvrt_word=bool(word_1)
+print(cnvrt_word,"\n##################")
+
+number_5=6
+cnvrt_5=str(number_5)
+print(type(cnvrt_5))
+
+# number_6="123abc"
+# cnvrt_6=int(number_6)
+# print(cnvrt_6,"\n##################")
+
+number_7=5.66
+cnvrt_7_int=int(number_7)
+cnvrt_7_str=str(number_7)
+print("The integer is ",cnvrt_7_int," and the string is", cnvrt_7_str)
+
+literal_1="Learning python is not a fun!"
+print(literal_1,"\n##################")
+
+literal_int, literal_float = 6, 10.5
+print(literal_int,literal_float,"\n##################")
+
+literal_single_quotes='Hellooo G!'
+literal_double_quotes="byyyy G!"
+print(literal_single_quotes,"\n",literal_double_quotes,"\n##################")
+
+literal_bool_1=True
+literal_bool_2=False
+print(literal_bool_1,"\n",literal_bool_2,"\n##################")
+
+var_value=None
+print(var_value,"\n##################")
+
+literal_2="Helloo"
+literal_3=10
+literal_4=5.3
+literal_5=True
+literal_6=None
+print(type(literal_2),type(literal_3),type(literal_4),type(literal_5),type(literal_6),"\n##################")
+
+literal_7="python simplicity is powerful!"
+print(literal_7,"\n##################")
+
+literal_str_1="Hey! This is "
+literal_str_2="Salman Khan"
+print(literal_str_1+literal_str_2,"\n##################")
+
+mixed_literals=[5,'salman Khan', True]
+print(mixed_literals,"\n##################")
+
+multi_literal=""" Hello, World!
+Name: Salman Khan, Age: 21, City: Nowshera
+welcome to pytho!
+Let's learn coding
+He said, "Python is Amazing!""" 
+print(multi_literal,"\n##################")
+
+print(None==0)
+print(None==False)
+
+
+user_name=input("Enter your Name:")
+print("Hello,{user_name}\n##################")
+
+num_1=int(input("Enter num 1:"))
+num_2=int(input("Enter num 2:"))
+sum=num_1+num_2
+print("The sum is {sum} \n##################")
+
+fvt_color=input("Enter u r favourite color plzz:")
+print("Your favt color is {fvt_color} \n##################")
+
+your_city=input("plzz Enter Your City:")
+your_country=input("Plzz Enter your Country:")
+print("You live in {your_city} , {your_country}\n##################")
+
+your_age=int(input("Plzz enter your age:"))
+print("You are {your_age} years old\n##################")
+
+num3=int(input("Enter num 1 : "))
+num4=int(input("Enter num 2 : "))
+differ=num3-num4
+print("The difference is {differ}\n##################")
+
+word=str(input("Enter a word :"))
+print(word.upper(),"\n##################")
+
+sentence=str(input("Enter a sentence : "))
+print(sentence.length(),"\n##################")
+
+first_name=input("Enter your First Name : ")
+second_name=input("Enter your Second Name : ")
+concat=first_name+second_name
+print(concat,"\n##################")
+
+user_age=int(input("Enter Your Age plzz: "))
+print("Your age will be after five years is ",user_age+5,"\n##################")
+
+num5=int(input("Enter num 1:"))
+num6=int(input("Enter num 2:"))
+product=num5*num6
+print(product,"\n##################")
+
+namee=input("Enter your name")
+hobby=input("Enter your hobby")
+print("{namee} likes {hobby}\n################## ")
+
+num_7=input("Enter a number :")
+cnvrt_num_7=int(num_7)
+print(cnvrt_num_7*2,"\n##################")
+
+num_8=float(input("Enter a Floating number:"))
+cnvrt_num_8=int(num_8)
+print("The Original floating value is {num_8} and the converted value is {cnvrt_num_8} \n##################")
+
+num_9=int(input("Enter a num 1 : "))
+num_10=int(input("Enter a num 2 : "))
+adding_9_10=num_9+num_10
+print(adding_9_10,"\n##################")
+
+num_11=input("Enter a floating number : ")
+cnvrt_11_flt=float(num_11)
+cnvrt_11_int=int(num_11)
+print("The Float num is {cnvrt_11_flt} and the int is {cnvrt_11_int}\n##################")
+
+num_0=input("Enter a number or character")
+isnumeric=num_0.isnumeric()
+print(isnumeric)
